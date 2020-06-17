f399974ce0c56a1350c73c6adc2c94f12f33e418
Marcos Baia <cs.marcosbaia@gmail.com>
2020-06-17 18:02:52 -0300

    Update aula_07.4_exemplo.php

--- aula_07.4_exemplo.php
+++ aula_07.4_exemplo.php
@@ -8,7 +8,7 @@
 		<?php
             //Mostre se um eleitor é obrigado a votar ou não.
             $ano = $_GET["a"];
-            $idade = 2020 - $ano;
+            $idade = date("Y") - $ano;
 
             echo "Pessoas nascidas no ano de $ano tem $idade anos de idade <br/>";
             echo "Seu voto é ".(($idade >= 18 && $idade < 65) ? "OBRIGATÓRIO" : "NÃO OBRIGATÓRIO");
