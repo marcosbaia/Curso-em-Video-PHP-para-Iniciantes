8ed6b8b549d3cdfb91251f4c05a91800b6f4d109
Marcos Baia <cs.marcosbaia@gmail.com>
2020-06-16 23:39:03 -0300

    Create aula_04_variaveis.php

---
+++ aula_04_variaveis.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Aula 04 - Variáveis</title>
+	</head>
+	<body>
+		<h1>Testando PHP</h1>
+		<?php
+			echo "<h2>Olá, <br/> Mundo</h2>";
+		?>
+	</body>
+</html>
