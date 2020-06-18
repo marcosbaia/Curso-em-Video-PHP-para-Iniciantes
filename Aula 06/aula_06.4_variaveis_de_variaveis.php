1245c0b2088e758e2fe85b8b849a2aa5389c423e
Marcos Baia <cs.marcosbaia@gmail.com>
2020-06-18 01:18:15 -0300

    Create aula_06.4_variaveis_de_variaveis.php

---
+++ Aula 06/aula_06.4_variaveis_de_variaveis.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Aula 06 de PHP</title>
+    </head>
+	<body>
+		<?php
+            $alfabeto = "abc";
+            $$alfabeto = "123";
+            echo "O conteudo da variável ALFABETO é: $alfabeto <br/>";
+            echo "O conteudo da variável criada ABC é: $abc";
+		?>
+	</body>
+</html>
