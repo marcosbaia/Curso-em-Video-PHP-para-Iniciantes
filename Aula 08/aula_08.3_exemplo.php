c716422dcbe151e1fd3995c5bfe7384738ed190d
Marcos Baia <cs.marcosbaia@gmail.com>
2020-06-18 01:35:22 -0300

    Create aula_08.3_exemplo.php

---
+++ Aula 08/aula_08.3_exemplo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+        <?php
+            $txt = isset($_GET["t"]) ? $_GET["t"] : "Texto Generico";
+            $tam = isset($_GET["tam"]) ? $_GET["tam"] : "12pt";
+            $cor = isset($_GET["cor"]) ? $_GET["cor"] : "#000000";
+        ?>
+		<meta charset="UTF-8"/>
+        <title>Aula 08 de PHP</title>
+        <style>
+            span.texto {
+                font-size: <?php echo $tam; ?>;
+                color: <?php echo $cor; ?>;
+            }
+        </style>
+	</head>
+	<body>
+        <?php
+            echo "<span class='texto'>$txt</span>";
+        ?>
+	</body>
+</html>
